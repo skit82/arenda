4b5f57b58261a7ffe84d19a8c4aef7a7227a5335
Aleksey <a.abrosimow2012@yandex.ru>
2022-09-14 21:55:16 +0300

    Добавляем блок Вопросы

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {initCustomSelect} from './modules/form/init-custom-select';
 import {initFormValidate} from './modules/form/init-form-validate';
+import {initAccordions} from './modules/init-accordion';
 
 // ---------------------------------
 
@@ -19,8 +20,9 @@ window.addEventListener('DOMContentLoaded', () => {
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
     initModals();
-    initCustomSelect();
+    // initCustomSelect();
     initFormValidate();
+    initAccordions();
   });
 });
 
